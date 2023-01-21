8e21234166bfbe30e4f8b53d4a0095d30f641450
Marcos Couto <marcoscouto1997@gmail.com>
2023-01-21 17:35:30 -0300

    versao com o requisito chat

--- scripts.js
+++ scripts.js
@@ -1,58 +1,70 @@
-let mensagens = [];
+let mensagens = []; // array que vai receber as mensagens do servidor
 
-let nomeUsuario;
+let nomeUsuario; // vai armazenar o nome do prompt
 
-function buscarMensagens() {
-    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+let objNome; // objeto com o nome de usuario que vai ser enviado pro servidor
 
-    console.log(promise);
-    promise.then(chegou);
-    promise.catch(naoChegou);
-    
-}
+let novaMensagem; // objeto com as novas mensagens que serão enviadas pro servidor
+
+const lista = document.querySelector('ul');
+
 
 function exibirMensagemTela() {
-    const lista = document.querySelector('ul');
+
+    lista.innerHTML = '';
 
     for (let i = 0; i < mensagens.length; i++) {
         
-        let template = `
-            <li data-test="message" class="msg-tela">
-            (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
-            </li>
-        `
+        if (mensagens[i].type === "message") {
+            let template = `
+                <li data-test="message" class="message msg">
+                (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
+                </li>
+            `
+            lista.innerHTML += template;
+        }   
+        
+        if (mensagens[i].type === "status") {
+            let status = `
+                <li data-test="message" class="status msg">
+                (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text}
+                </li>
+            `
+            lista.innerHTML += status;
+        }
 
-        lista.innerHTML += template;
+        if (mensagens[i].type === "private_message" && (mensagens[i].from === nomeUsuario || mensagens[i].to === nomeUsuario)) {
+            let private = `
+                <li data-test="message" class="private-message msg">
+                (${mensagens[i].time}) ${mensagens[i].from} reservadamente para ${mensagens[i].to}: ${mensagens[i].text}
+                </li>
+            `
+            lista.innerHTML += private;
+        }
     }
-    
-    console.log(template);
+
+    document.querySelector('.msg:last-child').scrollIntoView();
 }
 
-buscarMensagens();
+// busca mensagens no servidor
+function buscarMensagens() {
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 
-function chegou(resposta) {
-    console.log('chegou');
+    promise.then(buscouMensagem);
+    promise.catch(naoBuscouMensagem);
+}
+
+function buscouMensagem(resposta) {
+    console.log('chegou requerimento para buscar mensagens');
     console.log(resposta.data);
     mensagens = resposta.data;
     exibirMensagemTela();
 }
 
-function naoChegou(erro) {
-    console.log('não chegou');
+function naoBuscouMensagem(erro) {
+    console.log('não chegou requerimento para buscar mensagens');
 }
 
+setInterval(buscarMensagens, 3000);
 
-function enviarNomeUsuario() {
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", nomeUsuario);
-
-    promise.then(enviado);
-    promise.catch(naoEnviado);
-}
-
-function enviado(resposta) {
-    console.log('chegou');
-}
-
-function naoEnviado(erro) {
-    console.log('nao chegou');
-}
+buscarMensagens();
