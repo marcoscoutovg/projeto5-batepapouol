e0e397d15095c5c8838d84c86d6e8b99f83d829c
Marcos Couto <marcoscouto1997@gmail.com>
2023-01-21 19:38:14 -0300

    versao com ajustes

--- scripts.js
+++ scripts.js
@@ -9,6 +9,30 @@ let novaMensagem; // objeto com as novas mensagens que serão enviadas pro servi
 const lista = document.querySelector('ul');
 
 
+
+
+function entrarNaSala() {
+    nomeUsuario = prompt("Digite seu nome");
+    objNome = {name: nomeUsuario};
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objNome);
+
+    promise.then(enviouNomeUsuario);
+    promise.catch(naoEnviouNomeUsuario);
+}
+
+function enviouNomeUsuario(resposta) {
+    console.log('chegou o nome de usuario');
+    buscarMensagens();
+}
+
+function naoEnviouNomeUsuario(erro) {
+    console.log('nao chegou o nome de usuario');
+    entrarNaSala();
+}
+
+entrarNaSala();
+
 function exibirMensagemTela() {
 
     lista.innerHTML = '';
@@ -18,7 +42,7 @@ function exibirMensagemTela() {
         if (mensagens[i].type === "message") {
             let template = `
                 <li data-test="message" class="message msg">
-                (${mensagens[i].time}) ${mensagens[i].from} para ${mensagens[i].to}: ${mensagens[i].text}
+                    <p>(${mensagens[i].time}) <span>${mensagens[i].from}</span> para <span>${mensagens[i].to}</span>: ${mensagens[i].text}</p>
                 </li>
             `
             lista.innerHTML += template;
@@ -27,7 +51,7 @@ function exibirMensagemTela() {
         if (mensagens[i].type === "status") {
             let status = `
                 <li data-test="message" class="status msg">
-                (${mensagens[i].time}) ${mensagens[i].from} ${mensagens[i].text}
+                    <p>(${mensagens[i].time}) <span>${mensagens[i].from}</span> ${mensagens[i].text}</p>
                 </li>
             `
             lista.innerHTML += status;
@@ -36,7 +60,7 @@ function exibirMensagemTela() {
         if (mensagens[i].type === "private_message" && (mensagens[i].from === nomeUsuario || mensagens[i].to === nomeUsuario)) {
             let private = `
                 <li data-test="message" class="private-message msg">
-                (${mensagens[i].time}) ${mensagens[i].from} reservadamente para ${mensagens[i].to}: ${mensagens[i].text}
+                    <p>(${mensagens[i].time}) <span>${mensagens[i].from}</span> reservadamente para <span>${mensagens[i].to}</span>: ${mensagens[i].text}</p>
                 </li>
             `
             lista.innerHTML += private;
@@ -66,27 +90,6 @@ function naoBuscouMensagem(erro) {
 }
 
 
-
-function entrarNaSala() {
-    nomeUsuario = prompt("Digite seu nome");
-    objNome = {name: nomeUsuario};
-
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objNome);
-
-    promise.then(enviouNomeUsuario);
-    promise.catch(naoEnviouNomeUsuario);
-}
-
-function enviouNomeUsuario(resposta) {
-    console.log('chegou o nome de usuario');
-}
-
-function naoEnviouNomeUsuario(erro) {
-    console.log('nao chegou o nome de usuario');
-    window.location.reload(); // trocar depois
-}
-
-
 function verificaConexao() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", objNome)
 
@@ -96,10 +99,12 @@ function verificaConexao() {
 
 function chegouConexao(resposta) {
     console.log('online');
+    console.log(resposta.data)
 }
 
 function naoChegouConexao(erro) {
     console.log('offline');
+    console.log(erro.response.status)
 }
 
 function enviarMensagem(){
@@ -115,6 +120,7 @@ function enviarMensagem(){
 
     promise.then(chegouMensagem);
     promise.catch(naoChegouMensagem);
+
 }
 
 function chegouMensagem(resposta) {
@@ -128,8 +134,4 @@ function naoChegouMensagem(erro) {
 
 setInterval(verificaConexao, 5000);
 
-entrarNaSala();
-
 setInterval(buscarMensagens, 3000);
-
-buscarMensagens();
