6c5b914bd962540e33620376a55a3269fb23863d
Marcos Couto <marcoscouto1997@gmail.com>
2023-01-23 12:30:16 -0300

    versao com ajustes 5.0

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // array que vai receber as mensagens do servidor
 let mensagens = [];
 
-// vai armazenar o nome do prompt
+// vai armazenar o nome inserido no prompt
 let nomeUsuario; 
 
 // objeto com o nome de usuario que vai ser enviado pro servidor
@@ -26,7 +26,6 @@ function logar() {
 logar();
 
 function entrarNaSala() {
-
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objNome);
 
     promise.then(enviouNomeUsuario);
@@ -34,12 +33,12 @@ function entrarNaSala() {
 }
 
 function enviouNomeUsuario(resposta) {
-    console.log('chegou o nome de usuario');
     buscarMensagens();
+    setInterval(buscarMensagens, atualizaMensagem);
+    setInterval(verificaConexao, tempoVerificaConexao);
 }
 
 function naoEnviouNomeUsuario(erro) {
-    console.log('nao chegou o nome de usuario');
     alert("Digite outro nome. Esse já está em uso");
     logar();
     entrarNaSala();
@@ -71,10 +70,10 @@ function exibirMensagemTela() {
             lista.innerHTML += status;
         }
 
-        if (mensagens[i].type === "private_message" && (mensagens[i].from === nomeUsuario || mensagens[i].to === nomeUsuario)) {
+        if ((mensagens[i].type === "private_message" && mensagens[i].from === nomeUsuario) || (mensagens[i].type === "private_message" && mensagens[i].to === nomeUsuario)) {
             const privado = `
                 <li data-test="message" class="private-message msg">
-                    <p><span class="tempo">(${mensagens[i].time})</span>   a <span>${mensagens[i].from}</span> reservadamente para <span>${mensagens[i].to}</span>: ${mensagens[i].text}</p>
+                    <p><span class="tempo">(${mensagens[i].time})</span>     <span>${mensagens[i].from}</span> reservadamente para <span>${mensagens[i].to}</span>: ${mensagens[i].text}</p>
                 </li>
             `
             lista.innerHTML += privado;
@@ -93,36 +92,28 @@ function buscarMensagens() {
 }
 
 function buscouMensagem(resposta) {
-    console.log('chegou requerimento para buscar mensagens');
-    console.log(resposta.data);
     mensagens = resposta.data;
     exibirMensagemTela();
 }
 
 function naoBuscouMensagem(erro) {
-    console.log('não chegou requerimento para buscar mensagens');
+    window.location.reload();
 }
-
 
 function verificaConexao() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", objNome);
 
-    promise.then(chegouConexao);
+    promise.then();
     promise.catch(naoChegouConexao);
 }
 
-function chegouConexao(resposta) {
-    console.log('online');
-    console.log(resposta.data);
-}
-
 function naoChegouConexao(erro) {
-    console.log('offline');
-    console.log(erro.response.status);
+    window.location.reload();
 }
 
 function enviarMensagem(){
     const message = document.querySelector('textarea');
+    
     novaMensagem ={
         from: nomeUsuario,
         to: "Todos",
@@ -139,15 +130,9 @@ function enviarMensagem(){
 }
 
 function chegouMensagem(resposta) {
-    console.log('nova mensagem chegou no servidor');
     buscarMensagens();
 }
 
 function naoChegouMensagem(erro) {
-    console.log('nova mensagem não chegou no servidor');
     window.location.reload();
 }
-
-setInterval(verificaConexao, tempoVerificaConexao);
-
-setInterval(buscarMensagens, atualizaMensagem);
